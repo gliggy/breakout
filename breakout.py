268aeeb519c4a13482f5894eac852e4a90abf23b
gliggy <leocfitz@gmail.com>
2020-12-16 15:44:51 -0500

    add ball and bounce

--- breakout.py
+++ breakout.py
@@ -1,6 +1,7 @@
 # Importing the library 
 import pygame 
 import sys
+import random
   
 # Initializing Pygame 
 pygame.init() 
@@ -11,9 +12,10 @@ pygame.display.set_caption('Breakout')
 
 # Initializing surface 
 surface = pygame.display.set_mode((710,500)) 
+
+font = pygame.font.SysFont('Tahoma', 60, True, False)
   
 # Initialing Color 
-
 red = (255,0,0)
 orange = (255,128,0)
 yellow = (255,255,0)
@@ -21,31 +23,89 @@ green = (0,255,0)
 blue = (0,0,255)
 violet = (191,0,255) 
 
-def make_bricks(height, color):
+def make_bricks(height, color, number):
     count = 0
     left = 10
-    while count < 10:
-        pygame.draw.rect(surface, color, pygame.Rect(left, height, 60, 30))
+    for x in number:
+        if x == 1:
+            pygame.draw.rect(surface, color, pygame.Rect(left, height, 60, 30))
+        else:
+            pygame.draw.rect(surface, (0,0,0), pygame.Rect(left, height, 60, 30))
         left += 70
-        count += 1
+        
 
 def make_paddle(x_pos_new, x_pos_old):
     pygame.draw.rect(surface, (0,0,0), pygame.Rect(x_pos_old - 55, 480, 110, 10))
     pygame.draw.rect(surface, (255,255,0), pygame.Rect(x_pos_new - 55, 480, 110, 10))
 
+def make_ball(x_pos_new, x_pos_old, y_pos_new, y_pos_old):
+    pygame.draw.circle(surface, (0,0,0), (x_pos_old, y_pos_old), 20)
+    pygame.draw.circle(surface, (255,255,0), (x_pos_new, y_pos_new), 20)
+
+# set pos vars
 paddle_pos = 355
+ball_pos_x = 355
+ball_pos_y = 250
+x = random.choice([-3,3])
+y = random.randint(0,10)
+
+# set lives and score
+lives = 5
+score = 0
 
 while not done:  
-    # Drawing Rectangle 
-    make_bricks(10, red)
-    make_bricks(50, orange)
-    make_bricks(90, yellow)
-    make_bricks(130, green)
-    make_bricks(170, blue)
-    make_bricks(210, violet)
-
-
+    # Drawing Bricks 
+    brick_pos_y = [10,50,90,130,170,210]
     
+    red_array    = [1,1,1,1,0,1,1,1,1,1]
+    orange_array = [1,1,1,1,0,1,1,1,1,1]
+    yellow_array = [1,1,1,1,1,1,1,1,1,1]
+    green_array  = [1,1,1,0,1,1,1,1,1,1]
+    blue_array   = [1,1,1,1,1,1,0,1,1,1]
+    violet_array = [1,1,0,1,1,1,1,1,1,1]
+    
+    
+    make_bricks(brick_pos_y[0], red, red_array)
+    make_bricks(brick_pos_y[1], orange, orange_array)
+    make_bricks(brick_pos_y[2], yellow, yellow_array)
+    make_bricks(brick_pos_y[3], green, green_array)
+    make_bricks(brick_pos_y[4], blue, blue_array)
+    make_bricks(brick_pos_y[5], violet, violet_array)
+
+    if ball_pos_x + 20 > 710 or ball_pos_x - 20 < 0:
+        x = -x
+
+    # make the ball bounce
+    if ball_pos_y + 20 > 480:
+        if ball_pos_y + 20 < 490:
+            if paddle_pos - 50 < ball_pos_x < paddle_pos + 50:
+                y = -y
+        if ball_pos_y + 20 > 500:
+            lives -= 1
+            pygame.draw.circle(surface, (0,0,0), (ball_pos_x, ball_pos_y), 20)
+            ball_pos_x = 355
+            ball_pos_y = 250
+            x = random.choice([-3,3])
+            y = random.randint(1,10)
+    
+    if ball_pos_y - 20 < 0:
+        y = -y
+
+    if ball_pos_y < 220:
+        if 0 < ball_pos_x < 70 and violet_array[0] == 1:
+            violet_array[0] = 0
+
+
+
+    previous_ball_pos_x = ball_pos_x
+    previous_ball_pos_y = ball_pos_y
+    ball_pos_x += x
+    ball_pos_y += y
+    make_ball(ball_pos_x, previous_ball_pos_x, ball_pos_y, previous_ball_pos_y)
+
+    #print(lives)    
+    #display_lives = font.render(lives, True, (255,255,255))
+
 
     # draw and quit
     previous_paddle_pos = paddle_pos
@@ -54,10 +114,14 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if key_input[pygame.K_LEFT]:
-            paddle_pos -= paddle_speed
-        if key_input[pygame.K_RIGHT]:
-            paddle_pos += paddle_speed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                paddle_pos -= paddle_speed
+            if event.key == pygame.K_RIGHT:
+                paddle_pos += paddle_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                paddle_pos = paddle_pos
 
     make_paddle(paddle_pos, previous_paddle_pos)
     pygame.display.flip() 
---
+++ test3.py
@@ -0,0 +1,49 @@
+import pygame
+import sys
+from pygame.locals import *
+pygame.init()
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode((640, 400))
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect(x=x, y=y)
+
+player1, player2 = Player('square.png', 100, 100), Player('square1.png', 200, 200)
+players = pygame.sprite.Group(player1, player2)
+
+while True:
+    screen.fill(black)
+
+    if player1.rect.colliderect(player2.rect):
+        print ('collision!')
+
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_w:
+                player1.rect.move_ip(0, -10) 
+            elif event.key == K_s:
+                player1.rect.move_ip(0, 10)
+            elif event.key == K_a:
+                player1.rect.move_ip(-10, 0)
+            elif event.key == K_d:
+                player1.rect.move_ip(10, 0)
+            if event.key == K_UP:
+                player2.rect.move_ip(0, -10)
+            elif event.key == K_DOWN:
+                player2.rect.move_ip(0, 10)
+            elif event.key == K_LEFT:
+                player2.rect.move_ip(-10, 0)
+            elif event.key == K_RIGHT:
+                player2.rect.move_ip(10, 0)
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    players.draw(screen)
+    pygame.display.update()
