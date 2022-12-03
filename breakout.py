76d20f6da11194702ae92aece28f15a719a892ab
gliggy <leocfitz@gmail.com>
2022-12-03 12:19:39 -0500

    better bouncing

--- breakout.py
+++ breakout.py
@@ -66,40 +66,39 @@ def make_paddle(x_pos_new, x_pos_old):
     pygame.draw.rect(surface, (0,0,0), pygame.Rect(x_pos_old - 55, 480, 110, 10))
     pygame.draw.rect(surface, (255,255,0), pygame.Rect(x_pos_new - 55, 480, 110, 10))
 
-b = 0
+#b = 0
 
 def make_ball(x_pos_new, x_pos_old, y_pos_new, y_pos_old):
-    pixel = surface.get_at((int(x_pos_new), int(y_pos_new)))
-    if pixel != (255,255,0,255):
-        b = pixel[2]
+    #pixel = surface.get_at((int(x_pos_new), int(y_pos_new)))
+    #if pixel != (255,255,0,255):
+        #b = pixel[2]
         #print(b)
     pygame.draw.circle(surface, (0,0,0), (x_pos_old, y_pos_old), 20)
     pygame.draw.circle(surface, (255,255,0), (x_pos_new, y_pos_new), 20)
     return [x_pos_new,y_pos_new]
 
-def make_score(lives, score, ball_pos):
+def make_score(lives, score):
     pygame.draw.rect(surface, (0,0,0), pygame.Rect(0, 0, 710, 40))
     lives_write = font.render('Lives: {0}'.format(lives), 1, yellow)
     score_write = font.render('Score: {0}'.format(score), 1, yellow)
-    ball_pos_write = font.render('Ball pos: {0}'.format(ball_pos), 1, yellow)
     # score_write.fill(yellow)
     surface.blit(lives_write, (10, 10))
     surface.blit(score_write, (480, 10))
-    surface.blit(ball_pos_write, (200, 10))
 
 # set pos vars
 paddle_pos = 355
 ball_pos_x = 355
 ball_pos_y = 250
 
+# x and y are the incremental steps
 x = 0
 y = 0
 speed = 5
 
 def pick_direction():
-    angle = random.random()*math.pi*2
+    angle = (random.random()*0.6 + 0.2)*math.pi
     x = math.cos(angle)*speed
-    y = abs(math.sin(angle)*speed)
+    y = math.sin(angle)*speed
     return [x,y]
 [x,y] = pick_direction()
 
@@ -122,7 +121,7 @@ while not done:
     violet_array = colorArrays[5]
   
     # Drawing Bricks 
-    brick_pos_y = [70,110,150,190,230,270]    
+    brick_pos_y = [50,90,130,170,210,250]    
 
     make_bricks(brick_pos_y[0], red, red_array)
     make_bricks(brick_pos_y[1], orange, orange_array)
@@ -134,11 +133,16 @@ while not done:
     if ball_pos_x + 20 > 710 or ball_pos_x - 20 < 0:
         x = -x
 
-    # make the ball bounce
+    # make the ball bounce on the paddle
     if ball_pos_y + 20 > 480:
         if ball_pos_y + 20 < 490:
-            if paddle_pos - 50 < ball_pos_x < paddle_pos + 50:
+            if paddle_pos - 60 < ball_pos_x < paddle_pos + 60:
                 y = -y
+                if not(paddle_pos - 25 < ball_pos_x < paddle_pos + 25):
+                    hit_right = ball_pos_x - paddle_pos > 0
+                    moving_right = x > 0
+                    if hit_right != moving_right:
+                        x = -x
         if ball_pos_y + 20 > 500:
             lives -= 1
             pygame.draw.circle(surface, (0,0,0), (ball_pos_x, ball_pos_y), 20)
@@ -146,20 +150,23 @@ while not done:
             ball_pos_y = 250
             [x,y] = pick_direction()
     
-    make_score(str(lives), str(0), str(int(ball_pos_x))+" "+str(int(ball_pos_y)))
+    make_score(str(lives), str(0))
     
-    if ball_pos_y - 20 < 40 and red_array != [0,0,0,0,0,0,0,0,0,0]:
-        y = -y
-    elif ball_pos_y - 20 < 0:
+    #if ball_pos_y - 20 < 40 and red_array != [0,0,0,0,0,0,0,0,0,0]:
+    #    y = -y
+    if ball_pos_y < 0:
         y = -y
 
     # erase bricks
     i = 0
     while (i < 6):
-        if i == 0 and ball_pos_y < 70:
-            colorArrays[0][remove_brick(ball_pos_x)] = 0
-        elif brick_pos_y[i-1] < ball_pos_y < brick_pos_y[i]:
+        #if i == 0 and ball_pos_y < 70:
+        #    colorArrays[0][remove_brick(ball_pos_x)] = 0
+        if (brick_pos_y[i-1] if i else 0) < ball_pos_y < brick_pos_y[i]:
+            v = colorArrays[i][remove_brick(ball_pos_x)]
             colorArrays[i][remove_brick(ball_pos_x)] = 0
+            if v != 0:
+                y = -y
         i += 1
 
     previous_ball_pos_x = ball_pos_x
